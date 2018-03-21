8b23c439cb781d69663fa258bcf8e371ff011891
chakalakka <a.s.taudt@umcg.nl>
2018-03-21 10:02:11 +0100

    removed locale set

--- chromstaR_differences.R
+++ chromstaR_differences.R
@@ -8,9 +8,6 @@ options(show.error.messages=FALSE,
           }
         )
 
-# Avoid crashing Galaxy with an UTF8 error on German LC settings
-loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
-
 ## Parse input arguments
 library(optparse)
 option_list <- list(
